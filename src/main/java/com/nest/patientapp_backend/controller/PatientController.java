d6f1bf2b83ffa30330c72cba740aa7db7db9cba2
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-12-07 01:06:01 +0530

    add

--- src/main/java/com/nest/patientapp_backend/controller/PatientController.java
+++ src/main/java/com/nest/patientapp_backend/controller/PatientController.java
@@ -1,4 +1,27 @@
 package com.nest.patientapp_backend.controller;
 
+import com.nest.patientapp_backend.dao.PatientDao;
+import com.nest.patientapp_backend.model.Patient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class PatientController {
+    @Autowired
+    private PatientDao dao;
+
+    @CrossOrigin("*")
+    @GetMapping("/")
+    public String Homepage()
+    {
+        return "Home page";
+    }
+
+    @CrossOrigin("*")
+    @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
+    public String AddPatient(@RequestBody Patient p)
+    {
+        dao.save(p);
+        return "Added patient";
+    }
 }
